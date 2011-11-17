cf35c49b33ee6ebc0fc8317d98172a0deb6409a1
sagarandgaikwad@gmail.com <sagarandgaikwad@gmail.com@45af0448-5068-06e3-ea57-834d5875abd9>
2011-11-17 23:47:35 +0000

    This class will contain nested weblogo data structure.

---
+++ trunk/ nested-weblogo/NestedWebLogo/src/edu/ufl/cise/bioinformatics/nestedweblogo/NestedWebLogoDataStructure.java
@@ -0,0 +1,108 @@
+package edu.ufl.cise.bioinformatics.nestedweblogo;
+
+import edu.ufl.cise.bioinformatics.nestedweblogo.datastructure.WeblogoDataStructure;
+
+public class NestedWebLogoDataStructure {
+	
+	WeblogoDataStructure sourceWebLogo;
+	
+	WeblogoDataStructure targetWebLogo;
+	
+	private int sourceWebLogoStartPosition;
+	
+	private int sourceWebLogoEndPosition;
+	
+	private int targetWebLogoStartPosition;
+	
+	private int targetWebLogoEndPosition;
+	
+	private String wildCardPattern;
+
+	public WeblogoDataStructure getSourceWebLogo() {
+		return sourceWebLogo;
+	}
+
+
+
+	public void setSourceWebLogo(WeblogoDataStructure sourceWebLogo) {
+		this.sourceWebLogo = sourceWebLogo;
+	}
+
+
+
+	public WeblogoDataStructure getTargetWebLogo() {
+		return targetWebLogo;
+	}
+
+
+
+	public void setTargetWebLogo(WeblogoDataStructure targetWebLogo) {
+		this.targetWebLogo = targetWebLogo;
+	}
+
+
+
+	public int getSourceWebLogoStartPosition() {
+		return sourceWebLogoStartPosition;
+	}
+
+
+
+	public void setSourceWebLogoStartPosition(int sourceWebLogoStartPosition) {
+		this.sourceWebLogoStartPosition = sourceWebLogoStartPosition;
+	}
+
+
+
+	public int getSourceWebLogoEndPosition() {
+		return sourceWebLogoEndPosition;
+	}
+
+
+
+	public void setSourceWebLogoEndPosition(int sourceWebLogoEndPosition) {
+		this.sourceWebLogoEndPosition = sourceWebLogoEndPosition;
+	}
+
+
+
+	public int getTargetWebLogoStartPosition() {
+		return targetWebLogoStartPosition;
+	}
+
+
+
+	public void setTargetWebLogoStartPosition(int targetWebLogoStartPosition) {
+		this.targetWebLogoStartPosition = targetWebLogoStartPosition;
+	}
+
+
+
+	public int getTargetWebLogoEndPosition() {
+		return targetWebLogoEndPosition;
+	}
+
+
+
+	public void setTargetWebLogoEndPosition(int targetWebLogoEndPosition) {
+		this.targetWebLogoEndPosition = targetWebLogoEndPosition;
+	}
+
+
+
+	public String getWildCardPattern() {
+		return wildCardPattern;
+	}
+
+
+
+	public void setWildCardPattern(String wildCardPattern) {
+		this.wildCardPattern = wildCardPattern;
+	}
+
+
+
+	public static void main(String args[]){
+		
+	}
+}
